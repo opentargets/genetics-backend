d35093448b8cbadd88b4b7e0f8b09e608c060b3b
Miguel C <mkarmona@gmail.com>
2018-09-12 16:06:12 +0100

    feat: weighted and unweighted source scores

--- loaders/clickhouse/v2g.sql
+++ loaders/clickhouse/v2g.sql
@@ -75,7 +75,8 @@ as select
   max(ifNull(qtl_score_q, 0.)) AS max_qtl,
   max(ifNull(interval_score_q, 0.)) AS max_int,
   max(ifNull(fpred_max_score, 0.)) AS max_fpred,
-  (max_qtl + max_int + max_fpred) * dictGetFloat64('v2gw','weight',tuple(source_id)) as source_score
+  (max_qtl + max_int + max_fpred) as source_score,
+  (max_qtl + max_int + max_fpred) * dictGetFloat64('v2gw','weight',tuple(source_id)) as source_score_weighted
 from ot.v2g
 group by source_id, chr_id, variant_id, gene_id;
 
@@ -87,7 +88,7 @@ as select
   gene_id,
   groupArray(source_id) as source_list,
   groupArray(source_score) as source_score_list,
-  sum(source_score) / (select sum(weight) from ot.v2gw) as overall_score
+  sum(source_score_weighted) / (select sum(weight) from ot.v2gw) as overall_score
 from ot.v2g_score_by_source
 group by chr_id, variant_id, gene_id;
 
