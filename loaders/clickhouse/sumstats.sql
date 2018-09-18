f1dc4f4cad5645ea1918e95cc30ea469bbe0437f
Eliseo Papa <elipapa@users.noreply.github.com>
2018-09-18 01:03:20 +0100

    make a mt table per chromosome

--- loaders/clickhouse/sumstats.sql
+++ loaders/clickhouse/sumstats.sql
@@ -55,7 +55,7 @@ engine=Log;
 
 -- one per chromosome
 -- for chrom in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 'X', 'Y', 'MT']
-create table if not exists sumstats.gwas_chrMTtest
+create table if not exists sumstats.gwas_chr_1
 engine MergeTree partition by segment order by pos_b37
 as select
     cast(assumeNotNull(chip) as Enum8('genome_wide' = 1, 'immunochip' = 2, 'metabochip' = 3 )) as chip,
@@ -78,8 +78,8 @@ as select
     if(maf = 0, NULL ,maf) as maf,
     if(info = 0, NULL ,info) as info,
     if(is_cc = 'True', toUInt8(1), toUInt8(0)) as is_cc
-where chrom = 'MT'
-from sumstats.gwas_log;
+from sumstats.gwas_log
+where chrom = '1';
 
 --- test the fast new table with a typical phewas query:
 --- select study_id,trait_code,min(pval) from sumstats.gwas where chrom = '10' and variant_id_b37 = '10_102002678_G_A' and segment = 102 group by study_id,trait_code
@@ -114,33 +114,31 @@ engine=Log;
 
 
 
-create table if not exists sumstats.molecular_qtl
-engine MergeTree partition by (chrom) order by (pos_b37)
+create table if not exists sumstats.molecular_qtl_chr_1
+engine MergeTree partition by segment order by pos_b37
 as select
     CAST(assumeNotNull(experiment) AS Enum8('eqtl' = 1, 'pqtl' = 2, 'cell_counts' = 3, 'metabolites' = 4)) AS experiment,
     assumeNotNull(study_id) as study_id,
     assumeNotNull(tissue) as tissue,
     assumeNotNull(biomarker) as biomarker,
     assumeNotNull(variant_id_b37) as variant_id_b37,
-    assumeNotNull(chrom) as chrom,
+    cast(assumeNotNull(chrom) as Enum8('1' = 1, '2' = 2, '3' = 3, '4' = 4, '5' = 5, '6' = 6, '7' = 7, '8' = 8, '9' = 9, '10' = 10, '11' = 11, '12' = 12, '13' = 13, '14' = 14, '15' = 15, '16' = 16, '17' = 17, '18' = 18, '19' = 19, '20' = 20, '21' = 21, '22' = 22, 'X'= 23, 'Y' = 24, 'MT'=25 )) as chrom,
     assumeNotNull(pos_b37) as pos_b37,
     assumeNotNull(ref_al) as ref_al,
     assumeNotNull(alt_al) as alt_al,
     assumeNotNull(beta) as beta,
     assumeNotNull(se) as se,
     assumeNotNull(pval) as pval,
-    n_samples_variant_level,
-    -- if(n_samples_variant_level = 0, NULL ,n_samples_variant_level) as n_samples_variant_level,
-    n_samples_study_level,
-    n_cases_variant_level,
-    n_cases_study_level,
-    eaf,
-    maf,
-    info,
-
-    cast(is_cc AS UInt8('True' = 1, 'False' = 0)) as is_cc
-
-from sumstats.molecular_qtl_log;
+    if(n_samples_variant_level = 0, NULL ,n_samples_variant_level) as n_samples_variant_level,
+    if(n_samples_study_level = 0, NULL ,n_samples_study_level) as n_samples_study_level,
+    if(n_cases_study_level = 0, NULL ,n_cases_study_level) as n_cases_study_level,
+    if(n_cases_variant_level = 0, NULL ,n_cases_variant_level) as n_cases_variant_level,
+    if(eaf = 0, NULL ,eaf) as eaf,
+    if(maf = 0, NULL ,maf) as maf,
+    if(info = 0, NULL ,info) as info,
+    if(is_cc = 'True', toUInt8(1), toUInt8(0)) as is_cc
+from sumstats.molecular_qtl_log
+where chrom = '1';
 
 
 
