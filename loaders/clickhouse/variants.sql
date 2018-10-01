9793e39fdfd4d797253db752b62fc28a74e61ae2
Miguel C <mkarmona@gmail.com>
2018-10-01 18:13:03 +0100

    fix: close properly variants_log table

--- loaders/clickhouse/variants.sql
+++ loaders/clickhouse/variants.sql
@@ -1,5 +1,5 @@
 create database if not exists ot;
-create table if not exists ot.variants_log(
+create table if not exists ot.variants_log (
   chr_id String,
   position UInt32,
   ref_allele String,
@@ -7,7 +7,7 @@ create table if not exists ot.variants_log(
   variant_id String,
   rs_id String,
   gene_id_prot_coding String,
-  gene_id String,
+  gene_id String)
 engine = Log;
 
 create table if not exists ot.variants
