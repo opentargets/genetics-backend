8f6cbbcccad9d61e638bcf9d011ca173a889b58a
Miguel C <mkarmona@gmail.com>
2018-10-02 08:44:00 +0100

    fix: partition by chr and order by variant_id in the variants table

--- loaders/clickhouse/variants.sql
+++ loaders/clickhouse/variants.sql
@@ -11,7 +11,7 @@ create table if not exists ot.variants_log (
 engine = Log;
 
 create table if not exists ot.variants
-engine MergeTree partition by (chr_id) order by (position)
+engine MergeTree partition by (chr_id) order by (variant_id)
 as select
   assumeNotNull(chr_id) as chr_id,
   assumeNotNull(position) as position,
@@ -19,7 +19,7 @@ as select
   assumeNotNull(alt_allele) as alt_allele,
   assumeNotNull(variant_id) as variant_id,
   assumeNotNull(rs_id) as rs_id,
-  gene_id_prot_coding,
-  gene_id
+  gene_id,
+  gene_id_prot_coding
 from ot.variants_log;
 
