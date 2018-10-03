85c4c8950385b401aa48d4d26523b4d4f9da3cab
Miguel C <mkarmona@gmail.com>
2018-10-03 15:36:55 +0100

    fix: v2g structure must be there to recreate

---
+++ loaders/clickhouse/v2g_structure.sql
@@ -0,0 +1,17 @@
+create materialized view ot.v2g_structure
+engine=Memory populate as
+SELECT 
+    type_id,
+    source_id,
+    groupUniqArray(feature) AS feature_set,
+    length(feature_set) AS feature_set_size
+FROM ot.v2g
+WHERE chr_id = '1'
+GROUP BY 
+    type_id,
+    source_id
+ORDER BY 
+    type_id ASC,
+    source_id ASC,
+    feature_set ASC;
+
