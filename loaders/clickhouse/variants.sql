640d9d0af4b211d7d69766c6081b63e357e0fca4
Miguel C <mkarmona@gmail.com>
2019-01-30 13:17:40 +0000

    feat: variant index with annotations included, also partitioning by chr and segment

--- loaders/clickhouse/variants.sql
+++ loaders/clickhouse/variants.sql
@@ -1,13 +1,29 @@
 create table if not exists ot.variants
-engine MergeTree partition by (chr_id) order by (variant_id)
+engine MergeTree partition by (chr_id, segment) order by (variant_id)
 as select
   assumeNotNull(chr_id) as chr_id,
   assumeNotNull(position) as position,
+  segment,
   assumeNotNull(ref_allele) as ref_allele,
   assumeNotNull(alt_allele) as alt_allele,
   assumeNotNull(variant_id) as variant_id,
   assumeNotNull(rs_id) as rs_id,
   gene_id,
-  gene_id_prot_coding
+  gene_id_prot_coding,
+  most_severe_consequence,
+  raw,
+  phred,
+  gnomad_afr,
+  gnomad_seu,
+  gnomad_amr,
+  gnomad_asj,
+  gnomad_eas,
+  gnomad_fin,
+  gnomad_nfe,
+  gnomad_nfe_est,
+  gnomad_nfe_seu,
+  gnomad_nfe_nwe,
+  gnomad_nfe_onf,
+  gnomad_oth
 from ot.variants_log;
 
--- loaders/clickhouse/variants_log.sql
+++ loaders/clickhouse/variants_log.sql
@@ -2,10 +2,27 @@ create database if not exists ot;
 create table if not exists ot.variants_log (
   chr_id String,
   position UInt32,
+  segment UInt32 MATERIALIZED (intDiv(position,1000000)),
   ref_allele String,
   alt_allele String,
-  variant_id String,
+  variant_id String MATERIALIZED (concat(chr_id,'_',toString(position),'_',ref_allele,'_',alt_allele)),
   rs_id String,
   gene_id_prot_coding String,
-  gene_id String)
+  gene_id String,
+  most_severe_consequence String,
+  raw Float64,
+  phred Float64,
+  gnomad_afr Float64,
+  gnomad_seu Float64,
+  gnomad_amr Float64,
+  gnomad_asj Float64,
+  gnomad_eas Float64,
+  gnomad_fin Float64,
+  gnomad_nfe Float64,
+  gnomad_nfe_est Float64,
+  gnomad_nfe_seu Float64,
+  gnomad_nfe_nwe Float64,
+  gnomad_nfe_onf Float64,
+  gnomad_oth Float64
+  )
 engine = Log;
