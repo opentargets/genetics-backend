463bb2b8fa575d8865b37036f60f36685896abf7
Miguel C <mkarmona@gmail.com>
2019-03-26 14:11:22 +0000

    refactor: remove genes dictionary as it nos needed anymore

--- loaders/clickhouse/dictionaries.sql
+++ loaders/clickhouse/dictionaries.sql
@@ -1,15 +1,3 @@
-create table ot.gene (
-    gene_id String,
-    gene_name String,
-    biotype String,
-    chr String,
-    tss UInt32,
-    start UInt32,
-    end UInt32,
-    fwdstrand UInt8,
-    exons String
-) Engine = Dictionary(gene);
-
 create table ot.v2gw (
     source_id String,
     weight Float64
