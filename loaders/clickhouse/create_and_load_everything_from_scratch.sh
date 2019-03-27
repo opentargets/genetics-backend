ba7395a4b3e304da9bd228648cc92810b63fd5c4
Miguel C <mkarmona@gmail.com>
2019-03-27 09:56:41 +0000

    fix: latest version of the pipeline output

--- loaders/clickhouse/create_and_load_everything_from_scratch.sh
+++ loaders/clickhouse/create_and_load_everything_from_scratch.sh
@@ -3,7 +3,7 @@
 # CURRENTLY, IN ORDER TO BUILD SOME TABLES WE NEED A HIGHMEM MACHINE
 
 root_path=$(pwd)
-base_path="gs://genetics-portal-output/1903"
+base_path="gs://genetics-portal-output/190301"
 echo "loading file ${filename}"
 
 echo create genes table
