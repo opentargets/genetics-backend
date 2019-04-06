c6d242807613909feb23594d076937a9c3af2e1a
Miguel C <mkarmona@gmail.com>
2019-04-06 08:47:04 +0100

    feat: new minor data release

--- loaders/clickhouse/create_and_load_everything_from_scratch.sh
+++ loaders/clickhouse/create_and_load_everything_from_scratch.sh
@@ -3,7 +3,7 @@
 # CURRENTLY, IN ORDER TO BUILD SOME TABLES WE NEED A HIGHMEM MACHINE
 
 root_path=$(pwd)
-base_path="gs://genetics-portal-output/190302"
+base_path="gs://genetics-portal-output/190303"
 echo "loading file ${filename}"
 
 echo create genes table
