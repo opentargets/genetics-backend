fe4ab63b9d2e445852aef3ae613f79374c38757d
Ruslan Forostianov <ruslan@thehyve.nl>
2019-05-09 16:01:14 +0200

    Add option to clickhouse script load data from disk
    Other improveements:
    - Specify url or path to the data as external parameter.
    - Make script work even when it's executed from other folder.
    - Add posibility to specify elasticsearch hostname.
    - Add posibility to specify clickhouse hostname.
    - Add more verbose logging. More echo commands and showing running commands.

--- loaders/clickhouse/create_and_load_everything_from_scratch.sh
+++ loaders/clickhouse/create_and_load_everything_from_scratch.sh
@@ -1,81 +1,101 @@
 #!/bin/bash
 
 # CURRENTLY, IN ORDER TO BUILD SOME TABLES WE NEED A HIGHMEM MACHINE
+#set -e
+set -x
 
-root_path=$(pwd)
-base_path="gs://genetics-portal-output/190304"
-echo "loading file ${filename}"
+if [ $# -eq 0 ]; then
+    echo "Loads the opentargets genetics data to clickhouse database and elasticsearch."
+    echo "Usage: $0 google_storage_url_or_local_dir_location"
+    echo "Example 1: $0 /home/user/open-targets-genetics-releases/19.03.03"
+    echo "Example 2: $0 gs://open-targets-genetics-releases/19.03.03"
+    exit 1
+fi
 
+if [[ $1 == gs:* ]]; then
+   echo using Google Storage utils to read data
+   cat_cmd="gsutil cat"
+else
+   echo using local file system to read data
+   cat_cmd="cat"
+fi
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+base_path=${1}
+echo "loading files from ${base_path}"
+es_host="${ES_HOST:-localhost}"
+clickhouse_host="${CLICKHOUSE_HOST:-localhost}"
+
+echo "loading data to clikhouse with host ${clickhouse_host}"
 echo create genes table
-clickhouse-client -m -n < genes.sql
-gsutil cat "${base_path}/lut/genes-index/part-*" | clickhouse-client -h 127.0.0.1 --query="insert into ot.genes format JSONEachRow "
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/genes.sql
+"${cat_cmd}" ${base_path}/lut/genes-index/part-* | clickhouse-client -h "${clickhouse_host}" --query="insert into ot.genes format JSONEachRow "
 
 echo create studies tables
-clickhouse-client -m -n < studies_log.sql
-gsutil cat "${base_path}/lut/study-index/part-*" | clickhouse-client -h 127.0.0.1 --query="insert into ot.studies_log format JSONEachRow "
-clickhouse-client -m -n < studies.sql
-clickhouse-client -m -n -q "drop table ot.studies_log;"
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/studies_log.sql
+"${cat_cmd}" ${base_path}/lut/study-index/part-* | clickhouse-client -h "${clickhouse_host}" --query="insert into ot.studies_log format JSONEachRow "
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/studies.sql
+clickhouse-client -h "${clickhouse_host}" -m -n -q "drop table ot.studies_log;"
 
 echo create studies overlap tables
-clickhouse-client -m -n < studies_overlap_log.sql
-gsutil cat "${base_path}/lut/overlap-index/part-*" | clickhouse-client -h 127.0.0.1 --query="insert into ot.studies_overlap_log format JSONEachRow "
-clickhouse-client -m -n < studies_overlap.sql
-clickhouse-client -m -n -q "drop table ot.studies_overlap_log;"
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/studies_overlap_log.sql
+"${cat_cmd}" ${base_path}/lut/overlap-index/part-* | clickhouse-client -h "${clickhouse_host}" --query="insert into ot.studies_overlap_log format JSONEachRow "
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/studies_overlap.sql
+clickhouse-client -h "${clickhouse_host}" -m -n -q "drop table ot.studies_overlap_log;"
 
 echo create dictionaries tables
-clickhouse-client -m -n < dictionaries.sql
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/dictionaries.sql
 
 echo create variants tables
-clickhouse-client -m -n < variants_log.sql
-gsutil cat "${base_path}/lut/variant-index/part-*" | clickhouse-client -h 127.0.0.1 --query="insert into ot.variants_log format JSONEachRow "
-clickhouse-client -m -n < variants.sql
-clickhouse-client -m -n -q "drop table ot.variants_log;"
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/variants_log.sql
+"${cat_cmd}" ${base_path}/lut/variant-index/part-* | clickhouse-client -h "${clickhouse_host}" --query="insert into ot.variants_log format JSONEachRow "
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/variants.sql
+clickhouse-client -h "${clickhouse_host}" -m -n -q "drop table ot.variants_log;"
 
 echo create d2v2g tables
-clickhouse-client -m -n < d2v2g_log.sql
-gsutil cat "${base_path}/d2v2g/part-*" | clickhouse-client -h 127.0.0.1 --query="insert into ot.d2v2g_log format JSONEachRow "
-clickhouse-client -m -n < d2v2g.sql
-clickhouse-client -m -n -q "drop table ot.d2v2g_log;"
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/d2v2g_log.sql
+"${cat_cmd}" ${base_path}/d2v2g/part-* | clickhouse-client -h "${clickhouse_host}" --query="insert into ot.d2v2g_log format JSONEachRow "
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/d2v2g.sql
+clickhouse-client -h "${clickhouse_host}" -m -n -q "drop table ot.d2v2g_log;"
 
 echo create v2d tables
-clickhouse-client -m -n < v2d_log.sql
-gsutil cat "${base_path}/v2d/part-*" | clickhouse-client -h 127.0.0.1 --query="insert into ot.v2d_log format JSONEachRow "
-clickhouse-client -m -n < v2d.sql
-clickhouse-client -m -n -q "drop table ot.v2d_log;"
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/v2d_log.sql
+"${cat_cmd}" ${base_path}/v2d/part-* | clickhouse-client -h "${clickhouse_host}" --query="insert into ot.v2d_log format JSONEachRow "
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/v2d.sql
+clickhouse-client -h "${clickhouse_host}" -m -n -q "drop table ot.v2d_log;"
 
 echo create v2g tables
-clickhouse-client -m -n < v2g_log.sql
-gsutil cat "${base_path}/v2g/part-*" | clickhouse-client -h 127.0.0.1 --query="insert into ot.v2g_log format JSONEachRow "
-clickhouse-client -m -n < v2g.sql
-clickhouse-client -m -n -q "drop table ot.v2g_log;"
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/v2g_log.sql
+"${cat_cmd}" ${base_path}/v2g/part-* | clickhouse-client -h "${clickhouse_host}" --query="insert into ot.v2g_log format JSONEachRow "
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/v2g.sql
+clickhouse-client -h "${clickhouse_host}" -m -n -q "drop table ot.v2g_log;"
 
 echo create v2g structure
-clickhouse-client -m -n < v2g_structure.sql
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/v2g_structure.sql
 
 #echo compute v2g_scored table
-#clickhouse-client -m -n < v2g_scored.sql
+#clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/v2g_scored.sql
 
 echo compute d2v2g_scored table
-clickhouse-client -m -n < d2v2g_scored.sql
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/d2v2g_scored.sql
 
 echo compute locus 2 gene table
-clickhouse-client -m -n < d2v2g_scored_l2g.sql
+clickhouse-client -h "${clickhouse_host}" -m -n < ${script_dir}/d2v2g_scored_l2g.sql
 
 # elasticsearch process
+echo "loading data to elasticsearch with host ${es_host}"
 echo load elasticsearch studies data
-curl -XDELETE localhost:9200/studies
-gsutil cat "${base_path}/lut/study-index/part-*" | elasticsearch_loader --index-settings-file index_settings_studies.json --bulk-size 10000 --index studies --type study json --json-lines -
+curl -XDELETE ${es_host}:9200/studies
+"${cat_cmd}" ${base_path}/lut/study-index/part-* | elasticsearch_loader --es-host http://${es_host}:9200 --index-settings-file ${script_dir}/index_settings_studies.json --bulk-size 10000 --index studies --type study json --json-lines -
 
 echo load elasticsearch genes data
-curl -XDELETE localhost:9200/genes
-gsutil cat "${base_path}/lut/genes-index/part-*" | elasticsearch_loader --index-settings-file index_settings_genes.json --bulk-size 10000 --index genes --type gene json --json-lines -
+curl -XDELETE ${es_host}:9200/genes
+"${cat_cmd}" ${base_path}/lut/genes-index/part-* | elasticsearch_loader --es-host http://${es_host}:9200 --index-settings-file ${script_dir}/index_settings_genes.json --bulk-size 10000 --index genes --type gene json --json-lines -
 
 echo load elasticsearch variants data
 for chr in "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "x" "y" "mt"; do
 	chrU=$(echo -n $chr | awk '{print toupper($0)}')
-	curl -XDELETE localhost:9200/variant_$chr
-	clickhouse-client -q "select * from ot.variants prewhere chr_id = '${chrU}' format JSONEachRow" | elasticsearch_loader --index-settings-file index_settings_variants.json --bulk-size 10000 --index variant_$chr --type variant json --json-lines -
+    echo "Updating index for ${chrU} chromosome."
+	curl -XDELETE ${es_host}:9200/variant_$chr
+	clickhouse-client -h "${clickhouse_host}" -q "select * from ot.variants prewhere chr_id = '${chrU}' format JSONEachRow" | elasticsearch_loader --es-host http://${es_host}:9200 --index-settings-file ${script_dir}/index_settings_variants.json --bulk-size 10000 --index variant_$chr --type variant json --json-lines -
 done
-
-
-
