9ebc92b13f54993d7e065e92bb09c949eea1f62f
Miguel C <mkarmona@gmail.com>
2019-05-16 13:47:26 +0100

    refactor: chromosome column takes quite lot space as it is defined so this should optimise it
    Using enum type as UInt8 so it should be reduce it by half and also better for partition purposes

--- loaders/clickhouse/v2g.sql
+++ loaders/clickhouse/v2g.sql
@@ -2,7 +2,7 @@ create database if not exists ot;
 create table if not exists ot.v2g
 engine MergeTree partition by (source_id, chr_id) order by (position)
 as select
-  assumeNotNull(chr_id) as chr_id,
+  cast(assumeNotNull(chr_id) as Enum8('1' = 1, '2' = 2, '3' = 3, '4' = 4, '5' = 5, '6' = 6, '7' = 7, '8' = 8, '9' = 9, '10' = 10, '11' = 11, '12' = 12, '13' = 13, '14' = 14, '15' = 15, '16' = 16, '17' = 17, '18' = 18, '19' = 19, '20' = 20, '21' = 21, '22' = 22, 'X'= 23, 'Y' = 24, 'MT'=25 )) as chr_id,
   assumeNotNull(position) as position,
   assumeNotNull(ref_allele) as ref_allele,
   assumeNotNull(alt_allele) as alt_allele,
