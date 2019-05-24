618763b4682c814550161564d5cf27dadbd926a4
Miguel C <mkarmona@gmail.com>
2019-05-24 22:54:08 +0100

    fix: chromosome name to enum optimisation has to filter by reference chromosomes and exclude de rest

--- loaders/clickhouse/variants_log.sql
+++ loaders/clickhouse/variants_log.sql
@@ -2,9 +2,10 @@ create database if not exists ot;
 create table if not exists ot.variants_log (
   chr_id String,
   position UInt32,
+  chr_id_b37 String,
+  position_b37 UInt32,
   ref_allele String,
   alt_allele String,
-  variant_id String MATERIALIZED (concat(chr_id,'_',toString(position),'_',ref_allele,'_',alt_allele)),
   rs_id String,
   gene_id_prot_coding String,
   gene_id_any String,
