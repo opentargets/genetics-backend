09efbe11c5b9c1785cde2777b92331b3d8abc0e0
Ruslan Forostianov <ruslan@thehyve.nl>
2019-05-27 15:28:21 +0200

    Revert "Remove partition on gwas_chr_* tables"
    This reverts commit ddbd5d412ad09695966035bd01f2b4b89d2eb4a5.

--- loaders/clickhouse/scripts/sumstats_gwas_makechrtables.sh
+++ loaders/clickhouse/scripts/sumstats_gwas_makechrtables.sh
@@ -5,7 +5,7 @@ clickhouse_host="${SUMSTATS_CLICKHOUSE_HOST:-localhost}"
 for chrom in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y MT; do
 echo "Building gwas table for $chrom chromosome"
 clickhouse-client -h "${clickhouse_host}" --query="create table if not exists sumstats.gwas_chr_$chrom
-engine MergeTree order by (segment, pos_b37)
+engine MergeTree partition by segment order by pos_b37
 as select
     cast(assumeNotNull(chip) as Enum8('genome_wide' = 1, 'immunochip' = 2, 'metabochip' = 3 )) as chip,
     assumeNotNull(study_id) as study_id,
