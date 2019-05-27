c99d22a1270f753995cf787b212ab70dd4310bae
Ruslan Forostianov <ruslan@thehyve.nl>
2019-05-27 16:37:11 +0200

    Stop doing upload of gwas in data in parallel
    clickhouse gives a socket timeout error

--- loaders/clickhouse/scripts/load_sumstats_gwas.sh
+++ loaders/clickhouse/scripts/load_sumstats_gwas.sh
@@ -61,7 +61,7 @@ CUT_CHIP_STUDY_TRAIT_TO_ENV_VARS='CHIP=`echo {} | cut -d/ -f 5`; STUDY=`echo {} 
 PREPEND_CHIP_STUDY_TRAIT_TO_LINE='sed -e "s/^/$CHIP\t$STUDY\t$TRAIT\t/" '
 STDIN_TO_CLICKHOUSE_TABLE="clickhouse-client -h ${clickhouse_host} --query=\"insert into sumstats.gwas_log format TabSeparated\""
 LOAD_GZ_FILE_CMD="${CUT_CHIP_STUDY_TRAIT_TO_ENV_VARS}; ${cat_cmd} {} | zcat | sed 1d |${PREPEND_CHIP_STUDY_TRAIT_TO_LINE} | ${STDIN_TO_CLICKHOUSE_TABLE}; echo {} | tee -a done.log;"
-${list_files} | tee inputlist.txt | xargs -P 16 -I {} sh -c "${LOAD_GZ_FILE_CMD}"
+${list_files} | tee inputlist.txt | xargs -I {} sh -c "${LOAD_GZ_FILE_CMD}"
 
 # The above imports all empty fields (integers or floats) as zeros rather than
 # NULL. It's probably ok for this set, since there should be no zero in the
