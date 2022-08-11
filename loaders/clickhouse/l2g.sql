f8efdc1c18296d8516c49ee1e3b4b9bb01d8ae79
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-08-11 11:26:24 +0100

    Add standardised variant ID columns
    We variously refer to variants as 'lead', 'tag', 'left', 'right', 'A', B' and
    breaks them down into chr, pos, alt, ref.
    
    This change adds additional columns which standardises some references
    and lists them as single entries 'chr_pos_ref_alt` so we can use those
    for indexing and searching.
    
    Following API and ETL updates we will drop the broken down versions
    altogether.

--- loaders/clickhouse/l2g.sql
+++ loaders/clickhouse/l2g.sql
@@ -1,9 +1,46 @@
 create database if not exists ot;
-create table if not exists ot.l2g_by_slg
-engine MergeTree order by (study_id, pos, ref, alt, gene_id, y_proba_full_model)
-as select * from ot.l2g_log;
-
+create table if not exists ot.l2g_by_slg engine MergeTree
+order by (
+        study_id,
+        pos,
+        ref,
+        alt,
+        gene_id,
+        y_proba_full_model
+    ) as
+select *
+from ot.l2g_log;
 create database if not exists ot;
-create table if not exists ot.l2g_by_gsl
-engine MergeTree order by (gene_id, y_proba_full_model, study_id, pos, ref, alt)
-as select * from ot.l2g_log;
+create table if not exists ot.l2g_by_gsl engine MergeTree
+order by (
+        gene_id,
+        y_proba_full_model,
+        study_id,
+        pos,
+        ref,
+        alt
+    ) as
+select *
+from ot.l2g_log;
+-- l2g_by_gsl
+ALTER TABLE l2g_by_gsl
+ADD COLUMN IF NOT EXISTS variant_id String DEFAULT concat(
+        cast(chrom as String),
+        '_',
+        cast(pos as String),
+        '_',
+        ref,
+        '_',
+        alt
+    );
+-- l2g_by_slg
+ALTER TABLE l2g_by_slg
+ADD COLUMN IF NOT EXISTS variant_id String DEFAULT concat(
+        cast(chrom as String),
+        '_',
+        cast(pos as String),
+        '_',
+        ref,
+        '_',
+        alt
+    );
--- loaders/clickhouse/v2d_coloc.sql
+++ loaders/clickhouse/v2d_coloc.sql
@@ -1,27 +1,112 @@
 create database if not exists ot;
-create table if not exists ot.v2d_coloc
-    engine MergeTree
-        partition by (left_chrom)
-        order by (left_chrom, left_pos, left_ref, left_alt, left_study, right_pos, right_ref, right_alt, right_study)
-as select
-    coloc_h0,
+create table if not exists ot.v2d_coloc engine MergeTree partition by (left_chrom)
+order by (
+        left_chrom,
+        left_pos,
+        left_ref,
+        left_alt,
+        left_study,
+        right_pos,
+        right_ref,
+        right_alt,
+        right_study
+    ) as
+select coloc_h0,
     coloc_h1,
     coloc_h2,
     coloc_h3,
     coloc_h4,
-    cast(assumeNotNull(left_chrom) as Enum8('1' = 1, '2' = 2, '3' = 3, '4' = 4, '5' = 5, '6' = 6, '7' = 7, '8' = 8, '9' = 9, '10' = 10, '11' = 11, '12' = 12, '13' = 13, '14' = 14, '15' = 15, '16' = 16, '17' = 17, '18' = 18, '19' = 19, '20' = 20, '21' = 21, '22' = 22, 'X'= 23, 'Y' = 24, 'MT'=25 )) as left_chrom,
+    cast(
+        assumeNotNull(left_chrom) as Enum8(
+            '1' = 1,
+            '2' = 2,
+            '3' = 3,
+            '4' = 4,
+            '5' = 5,
+            '6' = 6,
+            '7' = 7,
+            '8' = 8,
+            '9' = 9,
+            '10' = 10,
+            '11' = 11,
+            '12' = 12,
+            '13' = 13,
+            '14' = 14,
+            '15' = 15,
+            '16' = 16,
+            '17' = 17,
+            '18' = 18,
+            '19' = 19,
+            '20' = 20,
+            '21' = 21,
+            '22' = 22,
+            'X' = 23,
+            'Y' = 24,
+            'MT' = 25
+        )
+    ) as left_chrom,
     left_pos,
     left_ref,
     left_alt,
     left_study,
-    cast(assumeNotNull(left_type) as Enum8('eqtl' = 1, 'pqtl' = 2, 'gwas' = 3, 'sqtl' = 4)) as left_type,
+    concat(
+        cast(left_chrom as String),
+        '_',
+        cast(left_pos as String),
+        '_',
+        left_ref,
+        '_',
+        left_alt
+    ) as left_variant_id,
+    cast(
+        assumeNotNull(left_type) as Enum8('eqtl' = 1, 'pqtl' = 2, 'gwas' = 3, 'sqtl' = 4)
+    ) as left_type,
     coloc_n_vars,
-    cast(assumeNotNull(right_chrom) as Enum8('1' = 1, '2' = 2, '3' = 3, '4' = 4, '5' = 5, '6' = 6, '7' = 7, '8' = 8, '9' = 9, '10' = 10, '11' = 11, '12' = 12, '13' = 13, '14' = 14, '15' = 15, '16' = 16, '17' = 17, '18' = 18, '19' = 19, '20' = 20, '21' = 21, '22' = 22, 'X'= 23, 'Y' = 24, 'MT'=25 )) as right_chrom,
+    cast(
+        assumeNotNull(right_chrom) as Enum8(
+            '1' = 1,
+            '2' = 2,
+            '3' = 3,
+            '4' = 4,
+            '5' = 5,
+            '6' = 6,
+            '7' = 7,
+            '8' = 8,
+            '9' = 9,
+            '10' = 10,
+            '11' = 11,
+            '12' = 12,
+            '13' = 13,
+            '14' = 14,
+            '15' = 15,
+            '16' = 16,
+            '17' = 17,
+            '18' = 18,
+            '19' = 19,
+            '20' = 20,
+            '21' = 21,
+            '22' = 22,
+            'X' = 23,
+            'Y' = 24,
+            'MT' = 25
+        )
+    ) as right_chrom,
     right_pos,
     right_ref,
     right_alt,
+    concat(
+        cast(right_chrom as String),
+        '_',
+        cast(right_pos as String),
+        '_',
+        right_ref,
+        '_',
+        right_alt
+    ) as right_variant_id,
     right_study,
-    cast(assumeNotNull(right_type) as Enum8('eqtl' = 1, 'pqtl' = 2, 'gwas' = 3, 'sqtl' = 4)) as right_type,
+    cast(
+        assumeNotNull(right_type) as Enum8('eqtl' = 1, 'pqtl' = 2, 'gwas' = 3, 'sqtl' = 4)
+    ) as right_type,
     coloc_h4_h3,
     coloc_log2_h4_h3,
     is_flipped,
@@ -32,7 +117,62 @@ as select
     left_var_right_study_se,
     left_var_right_study_pval,
     left_var_right_isCC
-from (select * from ot.v2d_coloc_log where left_chrom in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y','MT') and
-                                           right_chrom in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y','MT') and
-                                           coloc_n_vars >= 200
-     );
+from (
+        select *
+        from ot.v2d_coloc_log
+        where left_chrom in (
+                '1',
+                '2',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '9',
+                '10',
+                '11',
+                '12',
+                '13',
+                '14',
+                '15',
+                '16',
+                '17',
+                '18',
+                '19',
+                '20',
+                '21',
+                '22',
+                'X',
+                'Y',
+                'MT'
+            )
+            and right_chrom in (
+                '1',
+                '2',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '9',
+                '10',
+                '11',
+                '12',
+                '13',
+                '14',
+                '15',
+                '16',
+                '17',
+                '18',
+                '19',
+                '20',
+                '21',
+                '22',
+                'X',
+                'Y',
+                'MT'
+            )
+            and coloc_n_vars >= 200
+    );
