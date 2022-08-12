15c91c901da1e56a89eccfc15cfa730477af859b
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-08-12 11:00:06 +0100

    Revert "Add standardised variant ID columns"
    This reverts commit f8efdc1c18296d8516c49ee1e3b4b9bb01d8ae79.

--- loaders/clickhouse/studies_overlap.sql
+++ loaders/clickhouse/studies_overlap.sql
@@ -1,38 +1,18 @@
 create database if not exists ot;
-create table if not exists ot.studies_overlap (
-  A_chrom String,
-  A_pos UInt32,
-  A_ref String,
-  A_alt String,
+create table if not exists ot.studies_overlap
+(
+  A_chrom    String,
+  A_pos      UInt32,
+  A_ref      String,
+  A_alt      String,
   A_study_id String,
-  concat(
-    cast(A_chrom as String),
-    '_',
-    cast(A_pos as String),
-    '_',
-    A_ref,
-    '_',
-    A_alt
-  ) as A_variant_id,
-  B_study_id String,
-  B_chrom String,
-  B_pos UInt32,
-  B_ref String,
-  B_alt String,
-  concat(
-    cast(B_chrom as String),
-    '_',
-    cast(B_pos as String),
-    '_',
-    B_ref,
-    '_',
-    B_alt
-  ) as B_variant_id,
-  AB_overlap UInt32,
-  A_distinct UInt32,
-  B_distinct UInt32
-) engine MergeTree
-order by (A_study_id, A_chrom, A_pos, A_ref, A_alt);
-insert into ot.studies_overlap
-select *
-from ot.studies_overlap_log;
+    B_study_id String,
+    B_chrom String,
+    B_pos UInt32,
+    B_ref String,
+    B_alt String,
+    AB_overlap UInt32,
+    A_distinct UInt32,
+    B_distinct UInt32)
+engine MergeTree order by (A_study_id, A_chrom, A_pos, A_ref, A_alt);
+insert into ot.studies_overlap select * from ot.studies_overlap_log;
--- loaders/clickhouse/v2d_sa_gwas.sql
+++ loaders/clickhouse/v2d_sa_gwas.sql
@@ -1,87 +1,26 @@
-create table if not exists ot.v2d_sa_gwas engine MergeTree partition by (chrom)
-order by (chrom, pos) as
-select cast(assumeNotNull(type_id) as Enum8('gwas' = 1)) as type_id,
-    study_id,
-    cast(
-        assumeNotNull(chrom) as Enum8(
-            '1' = 1,
-            '2' = 2,
-            '3' = 3,
-            '4' = 4,
-            '5' = 5,
-            '6' = 6,
-            '7' = 7,
-            '8' = 8,
-            '9' = 9,
-            '10' = 10,
-            '11' = 11,
-            '12' = 12,
-            '13' = 13,
-            '14' = 14,
-            '15' = 15,
-            '16' = 16,
-            '17' = 17,
-            '18' = 18,
-            '19' = 19,
-            '20' = 20,
-            '21' = 21,
-            '22' = 22,
-            'X' = 23,
-            'Y' = 24,
-            'MT' = 25
-        )
-    ) as chrom,
-    pos,
-    ref,
-    alt,
-    concat(
-        cast(chrom as String),
-        '_',
-        cast(pos as String),
-        '_',
-        ref,
-        '_',
-        alt
-    ) as variant_id,
-    eaf,
-    mac,
-    mac_cases,
-    info,
-    beta,
-    se,
-    pval,
-    n_total,
-    n_cases,
-    is_cc
+create table if not exists ot.v2d_sa_gwas
+    engine MergeTree partition by (chrom) order by (chrom, pos)
+as
+select
+       cast(assumeNotNull(type_id) as Enum8('gwas' = 1)) as type_id,
+       study_id,
+       cast(
+               assumeNotNull(chrom) as Enum8('1' = 1, '2' = 2, '3' = 3, '4' = 4, '5' = 5, '6' = 6, '7' = 7, '8' = 8, '9' = 9, '10' = 10, '11' = 11, '12' = 12, '13' = 13, '14' = 14, '15' = 15, '16' = 16, '17' = 17, '18' = 18, '19' = 19, '20' = 20, '21' = 21, '22' = 22, 'X'= 23, 'Y' = 24, 'MT'=25 )) as chrom,
+       pos,
+       ref,
+       alt,
+       eaf,
+       mac,
+       mac_cases,
+       info,
+       beta,
+       se,
+       pval,
+       n_total,
+       n_cases,
+       is_cc
 from (
-        SELECT *
-        FROM ot.v2d_sa_gwas_log
-        WHERE pval < 0.005
-            and chrom IN (
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6',
-                '7',
-                '8',
-                '9',
-                '10',
-                '11',
-                '12',
-                '13',
-                '14',
-                '15',
-                '16',
-                '17',
-                '18',
-                '19',
-                '20',
-                '21',
-                '22',
-                'X',
-                'Y',
-                'MT'
-            )
-    );
+    SELECT *
+    FROM ot.v2d_sa_gwas_log
+    WHERE pval < 0.005 and chrom IN ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'MT')
+);
